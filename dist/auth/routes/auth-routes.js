e5c16306a34133aa97975a3af4bdda29e22aad71
hit-buzz <hitbuzz70@gmail.com>
2023-07-20 23:52:42 +0530

    removed dist file

--- dist/auth/routes/auth-routes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.registerAuthRoutes = void 0;
-const auth_service_1 = require("../services/auth-service");
-const registerAuthRoutes = (app, passport) => {
-    app.post("/login", passport.authenticate("local"), auth_service_1.passportAuthenticate);
-};
-exports.registerAuthRoutes = registerAuthRoutes;
-//# sourceMappingURL=auth-routes.js.map
